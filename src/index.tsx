eeb99b25a2dedf0fc49dd07220b4afa8f53b2823
suKyoung Shin <sukyoung.dev@gmail.com>
2023-04-01 21:34:00 +0900

    refactor: styled-component 글로벌스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
+import { GlobalStyle } from 'components/@utils/global-style';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
